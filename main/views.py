95e555bb58c42bba722e31e135dfecaf1c8a4fdf
zligusi <tort24abcd@gmaill.com>
2026-07-21 20:20:30 +0200

    inital loop example for data in index.html

--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title':'Home',
+        'values':['some','Hello','123'],
+        'oleg':{
+            'car': 'BMW',
+            'age': 23 ,
+            'hobby' : 'football',
+        }
+    }
+    return render(request, 'main/index.html', data)
 
 def about(request):
-    return render(request , 'main/about.html')
+    return render(request , 'main/about.html', {'title':'About'})
