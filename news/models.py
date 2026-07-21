1bb84004d3b0c9cc9ffdea995386ff1735fef55b
zligusi <tort24abcd@gmaill.com>
2026-07-21 22:09:55 +0200

    new setings news/models.py

--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Artiles(models.Model):
+    title = models.CharField('Title', max_length=50 ,)
+    anons = models.CharField('anons', max_length=250 ,)
+    full_text = models.TextField('article')
+    data = models.DateTimeField('Data and time')
+    
+    def __str__ (self):
+        return self.title
